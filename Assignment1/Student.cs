6f14602686ac411d1aa262adfd14906e20eb9c27
RudolfHarras <32720245+RudolfHarras@users.noreply.github.com>
2017-11-04 13:54:27 +0100

    Add files via upload

---
+++ Assignment1/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assignment1
+{
+	public class Student {
+		public string Name { get; set; }
+		public string Jmbag { get; set; }
+		public Gender Gender { get; set; }
+		public Student(string name, string jmbag) {
+			Name = name;
+			Jmbag = jmbag;
+		}
+
+
+
+		public override bool Equals(object o) {
+			if (ReferenceEquals(o, null)) return false;
+			return Equals((Student)o);
+		}
+
+		public bool Equals(Student s) {
+			if (ReferenceEquals(s, null)) return false;
+			return s.Jmbag == this.Jmbag;
+		}
+
+		public override int GetHashCode() {
+			return this.Jmbag.GetHashCode();
+		}
+
+		public static bool operator ==(Student a, Student b) {
+			if (ReferenceEquals(a, null) || ReferenceEquals(b, null)) return false;
+			return a.Jmbag == b.Jmbag;
+		}
+
+		public static bool operator !=(Student a, Student b) {
+			if (ReferenceEquals(a, null) || ReferenceEquals(b, null)) return false;
+			return a.Jmbag != b.Jmbag;
+		}
+	}
+	
+	public enum Gender {
+		Male, Female
+	}
+
+}
